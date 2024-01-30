f199919749ad747f830f979ddf7cd1910c22387a
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-01-30 15:45:47 +0000

    APIS-6737 Upgrade Play to 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.22.0"
+  private val bootstrapVersion = "8.4.0"
   val apiDomainVersion = "0.11.0"
   val commonDomainVersion = "0.10.0"
   val appDomainVersion = "0.32.0"
@@ -14,17 +14,15 @@ object AppDependencies {
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
   val compileDeps = Seq(
-    "uk.gov.hmrc"    %% "bootstrap-backend-play-28"          % bootstrapVersion,
-    "uk.gov.hmrc"    %% "api-platform-application-domain"    % appDomainVersion,
-    "uk.gov.hmrc"    %% "api-platform-api-domain"            % apiDomainVersion,
-    "uk.gov.hmrc"    %% "internal-auth-client-play-28"       % "1.9.0"
+    "uk.gov.hmrc"    %% "bootstrap-backend-play-30"         % bootstrapVersion,
+    "uk.gov.hmrc"    %% "api-platform-application-domain"   % appDomainVersion,
+    "uk.gov.hmrc"    %% "api-platform-api-domain"           % apiDomainVersion,
+    "uk.gov.hmrc"    %% "internal-auth-client-play-30"      % "1.10.0"
   )
 
   val testDeps = Seq(
-    "uk.gov.hmrc"    %% "bootstrap-test-play-28"     % bootstrapVersion,
-    "org.mockito"                 %% "mockito-scala-scalatest"           % "1.17.29",
-    "org.scalatest"               %% "scalatest"                         % "3.2.17",
-    "uk.gov.hmrc"                 %% "api-platform-test-common-domain"   % commonDomainVersion,
-
+    "uk.gov.hmrc"    %% "bootstrap-test-play-30"            % bootstrapVersion,
+    "org.mockito"    %% "mockito-scala-scalatest"           % "1.17.29",
+    "uk.gov.hmrc"    %% "api-platform-test-common-domain"   % commonDomainVersion
   ).map(_ % "test, it")
 }
