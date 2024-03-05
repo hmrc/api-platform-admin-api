85d773507c8923d46327ca0b8f80802a541d3a17
John Green <john.green@digital.hmrc.gov.uk>
2024-03-05 08:54:55 +0000

    APIS-6870: Subproject for integration tests (#14)
    * APIS-6870: Subproject for integration tests
    
    * APIS-6870: Fix "it" settings
    
    * APIS-6870: Got overzealous with the DefaultBuildSettings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,11 @@
-
-import play.core.PlayVersion
-import play.sbt.PlayImport.*
-import sbt.Keys.libraryDependencies
-import sbt.*
+import sbt._
 
 object AppDependencies {
 
   private val bootstrapVersion = "8.4.0"
-  val apiDomainVersion = "0.11.0"
-  val commonDomainVersion = "0.10.0"
-  val appDomainVersion = "0.32.0"
+  private val apiDomainVersion = "0.11.0"
+  private val commonDomainVersion = "0.10.0"
+  private val appDomainVersion = "0.32.0"
 
   def apply(): Seq[ModuleID] = compileDeps ++ testDeps
 
@@ -22,7 +18,7 @@ object AppDependencies {
 
   val testDeps = Seq(
     "uk.gov.hmrc"    %% "bootstrap-test-play-30"            % bootstrapVersion,
-    "org.mockito"    %% "mockito-scala-scalatest"           % "1.17.29",
+    "org.mockito"    %% "mockito-scala-scalatest"           % "1.17.30",
     "uk.gov.hmrc"    %% "api-platform-test-common-domain"   % commonDomainVersion
-  ).map(_ % "test, it")
+  ).map(_ % "test")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,9 +6,9 @@ addSbtPlugin("uk.gov.hmrc"         % "sbt-auto-build"        % "3.20.0")
 addSbtPlugin("uk.gov.hmrc"         % "sbt-distributables"    % "2.5.0")
 addSbtPlugin("org.playframework"   % "sbt-plugin"            % "3.0.1")
 addSbtPlugin("org.scoverage"       % "sbt-scoverage"         % "2.0.9")
-addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("org.scalastyle"      % "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("org.scalameta"       % "sbt-scalafmt"          % "2.5.2")
-addSbtPlugin("ch.epfl.scala"      %% "sbt-scalafix"          % "0.11.1")
-addSbtPlugin("ch.epfl.scala"       % "sbt-bloop"             % "1.5.11")
+addSbtPlugin("ch.epfl.scala"       % "sbt-scalafix"          % "0.11.1")
+addSbtPlugin("ch.epfl.scala"       % "sbt-bloop"             % "1.5.15")
 
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
