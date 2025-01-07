10343db54ad016c75d62df8ead40a171e1c6ce88
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-01-07 13:12:52 +0000

    API-7947 - Remove tolerant read of application (#20)

--- it/test/uk/gov/hmrc/apiplatformadminapi/stubs/ThirdPartyOrchestratorConnectorStub.scala
+++ it/test/uk/gov/hmrc/apiplatformadminapi/stubs/ThirdPartyOrchestratorConnectorStub.scala
@@ -19,13 +19,15 @@ package uk.gov.hmrc.apiplatformadminapi.stubs
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 
+import play.api.libs.json.Json
 import play.api.test.Helpers._
 
+import uk.gov.hmrc.apiplatform.modules.applications.core.domain.models.ApplicationWithCollaboratorsFixtures
 import uk.gov.hmrc.apiplatform.modules.common.domain.models.{ApplicationId, ClientId}
 import uk.gov.hmrc.apiplatform.modules.tpd.session.domain.models.UserSessionId
 import uk.gov.hmrc.apiplatformadminapi.utils.WireMockExtensions
 
-trait ThirdPartyOrchestratorConnectorStub extends WireMockExtensions {
+trait ThirdPartyOrchestratorConnectorStub extends WireMockExtensions with ApplicationWithCollaboratorsFixtures {
 
   object GetApplication {
 
@@ -81,38 +83,7 @@ trait ThirdPartyOrchestratorConnectorStub extends WireMockExtensions {
       )
   }
 
-  private val applicationResponseBody =
-    s"""{
-       |  "id": "967226ae-46ca-4b71-a76c-72efbc402a9b",
-       |  "clientId": "tl68AJH3PA8kKe7H9gxIatou2UTK",
-       |  "gatewayId": "gateway-id",
-       |  "name": "Application Name",
-       |  "deployedTo": "PRODUCTION",
-       |  "collaborators": [],
-       |  "createdOn": "2023-11-22T14:56:57.833Z",
-       |  "grantLength": 30,
-       |  "redirectUris": [],
-       |  "access": {
-       |    "redirectUris": [],
-       |    "overrides": [],
-       |    "accessType": "STANDARD"
-       |  },
-       |  "state": {
-       |    "name": "TESTING",
-       |    "updatedOn": "2023-11-22T14:56:57.833Z"
-       |  },
-       |  "rateLimitTier": "BRONZE",
-       |  "blocked": false,
-       |  "trusted": false,
-       |  "ipAllowlist": {
-       |    "required": false,
-       |    "allowlist": []
-       |  },
-       |  "moreApplication": {
-       |    "allowAutoDelete": false,
-       |    "lastActionActor": "UNKNOWN"
-       |  }
-       |}""".stripMargin
+  private val applicationResponseBody = Json.toJson(standardApp).toString()
 
   private val userResponseBody =
     s"""{
